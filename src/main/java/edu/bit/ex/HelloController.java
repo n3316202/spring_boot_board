05d0cc32ccb3a8a825458a77555f8bfe5f047523
nqwrt <nqwrt@ymail.com>
2021-06-26 19:31:13 +0900

    commit 2

--- src/main/java/edu/bit/ex/HelloController.java
+++ src/main/java/edu/bit/ex/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+public class HelloController {
 
 	@GetMapping("/")
 	public String home() {
